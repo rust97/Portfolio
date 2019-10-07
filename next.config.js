9884f6e6141537c445ec30dcb1f399b76f939506
Rustam <rust_97@mail.ru>
2019-10-07 21:12:01 +0300

    правки

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
 const withCSS = require("@zeit/next-css");
 
-<<<<<<< HEAD
-const withMDX = require("@next/mdx")({
-  // parse mdx files
-  extension: /\.mdx?$/
-});
-
-const baseConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx"],
-  webpack(config) {
-    config.resolve.modules = [path.resolve("./src"), ...config.resolve.modules];
-    return config;
-  }
-};
-
-module.exports = [withCSS, withMDX].reduce((a, b) => b(a), baseConfig);
-=======
 module.exports = withCSS({});
->>>>>>> +contentful
